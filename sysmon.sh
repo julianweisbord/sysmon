d66b1881bf26d50877ada2a5db357f6788fade0d
julianweisbord <julianweisbord@gmail.com>
2015-09-01 16:10:52 -0700

    Made cpu output more concise.

--- sysmon.sh
+++ sysmon.sh
@@ -18,14 +18,25 @@ printf "| Epoch Time: $(date +%s)"
 printf "\n\n"
 printf "Host on: $(uname -a)\n\n"
 
-
-#get rid of some info in lscpu
-lscpu
+printf "CPU Information:\n"
+# #get rid of some info in lscpu
+# lscpu
 #get model name
 for line in $(grep -Fm 1 "$model name" /proc/cpuinfo)
 do
- printf "$line " 
+ printf "$line "
 done
 printf "\n"
 #gives number of cpus
-grep -i "physical id" /proc/cpuinfo | sort -u | wc -l
+printf "Total "
+grep -i "cpu cores" /proc/cpuinfo | sort -u
+lscpu | grep -i "Thread(s) per core:" | sort -u
+lscpu | grep -i "Architecture:\|CPU family:\|Core(s) per socket:\|CPU MHz\|L1d cache:\|L1i cache:\|L2 cache:\|L3 cache:" | sort -u
+# printf "Turbo Boost"
+#
+# # printf "Total Frequency: "
+
+for line in $(sudo lshw | grep -i "$cpu capacity" | sort -u)
+do
+  printf $line | grep -iT "MHz" | sort -u
+done
